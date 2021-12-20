2108767b713d3d15cc80fba11ee23dcff695bf80
Anahi Jurado <anahielizabethjurado@gmail.com>
2021-12-20 10:31:19 -0300

    terminado

---
+++ js/punto9.js
@@ -0,0 +1,9 @@
+const cantDias = parseInt( prompt ('Ingrese la cantidad de dias'));
+
+const sumaDias = Number *24;
+const sumaMinutos = sumaHoras *60;
+const sumaSegundos = sumaMinutos *60;
+
+alert(
+  `En ${dias} dias hay : ${horas} horas, ${minutos} minutos y ${segundos} segundos en totalas`
+);
